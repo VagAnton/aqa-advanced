7a864e2c8cf52bc80a97b970ca16cacd98afdab8
Anton Veselkov <veselkov.ant@gmail.com>
2026-07-13 13:08:11 +0300

    HomeWork#4

---
+++ DZ4/task3(for).js
@@ -0,0 +1,5 @@
+const number = 4;
+
+for (let i = 1; i <= 10; i++){
+    console.log (`${number} x ${i} = ${number * i}`);
+};
---
+++ DZ4/task3(while).js
@@ -0,0 +1,6 @@
+const number1 = 4;
+let number2 = 1;
+while (number2 <= 10){
+    console.log(`${number1} x ${number2} = ${number1 * number2}`);
+    number2++;
+};
